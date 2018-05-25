0924b9fcd7ab4220eabed1c42c1330e097abf723
deymons <39637313+deymons@users.noreply.github.com>
2018-05-26 01:53:07 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,31 @@
+{
+  "ClientId": 449014580077920266,
+  "Token": "NDQ5MDE0NTgwMDc3OTIwMjY2.Deeg7w.LStFJ1OmMuOG0VJje3luRG4jaLI",
+  "OwnerIds": [
+    270593252028055552
+  ],
+  "LoLApiKey": "f3e0f1adbabbeb598106130135028c04 ",
+  "GoogleApiKey": "AIzaSyBHZD2cQ-AfUKCdYteabjL_xlwMSJRrcYo",
+  "MashapeKey": "KR9GCWBsGHmshT4rUrr6UuKh8Knyp1PVk1ojsnH1iypbbTonMR",
+  "OsuApiKey": "",
+  "SoundCloudClientId": "",
+  "CleverbotApiKey": "",
+  "CarbonKey": "",
+  "Db": {
+    "Type": "sqlite",
+    "ConnectionString": "Data Source=data/NadekoBot.db"
+  },
+  "TotalShards": 1,
+  "PatreonAccessToken": "",
+  "PatreonCampaignId": "334038",
+  "RestartCommand": null,
+  "ShardRunCommand": "",
+  "ShardRunArguments": "",
+  "ShardRunPort": null,
+  "MiningProxyUrl": null,
+  "MiningProxyCreds": null,
+  "BotListToken": null,
+  "TwitchClientId": null,
+  "VotesToken": null,
+  "VotesUrl": null
+}
